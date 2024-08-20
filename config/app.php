6908d4ba1f03428b02f3f7ca63065791132b8ff0
sneidervrod1 <sovacar@udistrital.edu.co>
2024-08-20 08:16:39 -0500

    prod 7.2

--- config/app.php
+++ config/app.php
@@ -159,8 +159,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Spatie\Permission\PermissionServiceProvider::class,
-
         /*
          * Application Service Providers...
          */
